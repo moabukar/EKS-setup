17f8eda967b6c3a77ca36d5f92acb3dadd0f7826
Mohamed A <76791648+mohamedA007@users.noreply.github.com>
2021-08-16 23:52:53 +0100

    EKS setup

---
+++ eks-cluster.tf
@@ -0,0 +1,44 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.myapp-cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.myapp-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "myapp-cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "myapp-cluster" {
+  name = module.eks.cluster_id
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "13.2.1"
+
+  cluster_name    = "myapp-eks-cluster"
+  cluster_version = "1.17"
+
+  subnets = module.quarklink-vpc.private_subnets
+  vpc_id  = module.quarklink-vpc.vpc_id
+
+  tags = {
+    environment = "dev"
+    application = "myapp"
+  }
+
+  worker_groups = [
+    {
+      instance_type        = "t2.micro"
+      name                 = "worker-group-1"
+      asg_desired_capacity = 2
+    },
+    {
+      instance_type        = "t2.micro"
+      name                 = "worker-group-2"
+      asg_desired_capacity = 1
+    }
+  ]
+
+
+}
---
+++ vpc.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
+
+data "aws_availability_zones" "azs" {}
+
+module "quarklink-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  name            = "quarklink-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
+
+}
